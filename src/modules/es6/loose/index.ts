9ad3fb0f50d5ebff3504cbabbdd4ea71cb8c5f08
me <bhavesh.pareek36@gmail.com>
2019-09-22 13:11:32 +0530

    patch object property enumerating methods

--- src/modules/es6/loose/index.ts
+++ src/modules/es6/loose/index.ts
@@ -2,6 +2,7 @@ export { default as FakeMap } from "./Map/index.js";
 export { default as FakeSet } from "./Set/index.js";
 export {
   default as FakeWeakMap,
-  patchObjectSealingMethods
+  patchObjectSealingMethods,
+  patchPropertyDescriptorMethods
 } from "./WeakMap/index.js";
 export { default as FakeWeakSet } from "./WeakSet/index.js";
