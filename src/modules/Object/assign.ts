087744c72cbad44054a6254c7d5c322d99f70a5e
me <bhavesh.pareek36@gmail.com>
2020-10-28 12:37:19 +0530

    modern js only

--- src/modules/Object/assign.ts
+++ src/modules/Object/assign.ts
@@ -4,6 +4,7 @@ export default "assign" in _Object
   : (function Object_assign(target: import("./ot").default) {
       for (let i = 1; i < arguments.length; i++) {
         const source = arguments[i];
+        console.log(source);
         for (const key in source) {
           if (hasOwnProp.call(source, key)) {
             target[key] = source[key];
